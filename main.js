d87bf148c8bfcad00606861ba6d01404418b91ac
Santhana Kumar K <123801806+sandal-1321@users.noreply.github.com>
2023-03-12 11:03:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,74 @@
+let musi=document.querySelector("#mu1");
+let musi2=document.querySelector("#mu2");
+let musi3=document.querySelector("#mu3");
+let musi4=document.querySelector("#mu4");
+let musi5=document.querySelector("#mu5");
+let musi6=document.querySelector("#mu6");
+let musi7=document.querySelector("#mu7");
+let musi8=document.querySelector("#mu8");
+let musi9=document.querySelector("#mu9");
+let musi10=document.querySelector("#mu10");
+let musi11=document.querySelector("#mu11");
+let musi12=document.querySelector("#mu12");
+let musi13=document.querySelector("#mu13");
+let musi14=document.querySelector("#mu14");
+musi.addEventListener("click",function(){
+	var audio=new Audio('music/1.mp3');
+	audio.play();
+})
+musi2.addEventListener("click",function(){
+	var audio=new Audio('music/2.mp3');
+	audio.play();
+})
+musi3.addEventListener("click",function(){
+	var audio=new Audio('music/3.mp3');
+	audio.play();
+})
+musi4.addEventListener("click",function(){
+	var audio=new Audio('music/5.mp3');
+	audio.play();
+})
+musi5.addEventListener("click",function(){
+	var audio=new Audio('music/5.mp3');
+	audio.play();
+})
+musi6.addEventListener("click",function(){
+	var audio=new Audio('music/6.mp3');
+	audio.play();
+})
+musi7.addEventListener("click",function(){
+	var audio=new Audio('music/7.mp3');
+	audio.play();
+})
+musi7.addEventListener("click",function(){
+	var audio=new Audio('music/7.mp3');
+	audio.play();
+})
+musi8.addEventListener("click",function(){
+	var audio=new Audio('music/8.mp3');
+	audio.play();
+})
+musi9.addEventListener("click",function(){
+	var audio=new Audio('music/9.mp3');
+	audio.play();
+})
+musi10.addEventListener("click",function(){
+	var audio=new Audio('music/10.mp3');
+	audio.play();
+})
+musi11.addEventListener("click",function(){
+	var audio=new Audio('music/4.mp3');
+	audio.play();
+})
+musi12.addEventListener("click",function(){
+	var audio=new Audio('music/6.mp3');
+	audio.play();
+})
+musi13.addEventListener("click",function(){
+	var audio=new Audio('music/8.mp3');
+	audio.play();
+})
+musi14.addEventListener("click",function(){
+	var audio=new Audio('music/2.mp3');
+	audio.play();
+})
